7f9487b6284f1e2bd8b73956e9c52dde4d05036e
NattanitaA <61011294@kmitl.ac.th>
2021-07-14 16:07:39 +0700

    test1

--- test.js
+++ test.js
@@ -1,13 +1,1 @@
-console.log('test')
-
-var alphabet = ['a'];
-console.log(alphabet)
-
-console.log('testbam')
-console.log('testbambam')
-console.log('blabla')
-console.log('babla')
-console.log('bammmm')
-
-
-console.log("iceiceice")
+console.log('test1')
