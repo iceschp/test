9d2e5443d20e794685a7b05eba130a2722aa6774
NattanitaA <61011294@kmitl.ac.th>
2021-07-14 16:05:28 +0700

    no message

--- test-ice.js
+++ test-ice.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 console.log("test-ice")
-=======
-console.log("ice test test 15:42")
->>>>>>> yim
--- test.js
+++ test.js
@@ -7,10 +7,4 @@ console.log('testbam')
 console.log('testbambam')
 console.log('blabla')
 console.log('babla')
-<<<<<<< HEAD
 console.log('bammmm')
-=======
-console.log('babla')
-
-rgjtngjtrgnirjt
->>>>>>> yim
