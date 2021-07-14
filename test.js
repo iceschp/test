c63f9827fba2f6694e8da8a35b5e594efdb1aa84
iceschp <warunya.ks@gmail.com>
2021-07-14 15:32:39 +0700

    ice commit 15:32

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
 console.log('test')
-<<<<<<< HEAD
 
 var alphabet = ['a'];
 console.log(alphabet)
-=======
+
 console.log('testbam')
 console.log('testbambam')
 console.log('blabla')
 console.log('babla')
 console.log('babla')
->>>>>>> refs/remotes/origin/main
